f14adcf40afad80d37e57402f4eeccba9d37f0ad
land5cape <2548983377@qq.com>
2020-12-02 21:53:36 +0800

    1.0

--- test_threadpool_2_run.ps1
+++ test_threadpool_2_run.ps1
@@ -1,12 +1,13 @@
 # powershell thread pool
-# http://www.pstips.net/speeding-up-powershell-multithreading.html
+Set-Location "E:\workplace\VC_bat" # 执行ps脚本需要切换到当前路径
 $tm_start = Get-Date
 Write-Host '任务开始于：' + (Get-Date -Format 'yyyy-MM-dd HH:mm:ss')
 $throttleLimit = 4
 $SessionState = [system.management.automation.runspaces.initialsessionstate]::CreateDefault()
-$Pool = [runspacefactory]::CreateRunspacePool(1, $throttleLimit, $SessionState, $Host)
+$Pool = [runspacefactory]::CreateRunspacePool(1, $throttleLimit, $SessionState, $Host) #设置Runspace数量(1，4)
 $Pool.Open()
 
+# {}定义脚本块
 $ScriptBlock = {
     param($id)
     Write-Host "Start Task ID $id." -foregroundcolor Cyan -backgroundcolor DarkBlue
@@ -15,26 +16,26 @@ $ScriptBlock = {
     $exe_id     = $file_cont[-3]
     $exe_params = $file_cont[-2]
     $exe_output = $file_cont[-1]
-    cmd /c del /Q $exe_output
-    Write-Host "Task [$id]: $exe_params" -ForegroundColor Green 
-    Write-Host "Task [$id]: /output:$exe_output" -ForegroundColor Red
+    cmd /c del /Q $exe_output # cmd /c 执行cmd命令 删除旧的log文件
+    Write-Host "Task [$exe_id]: $exe_params" -ForegroundColor Green 
+    Write-Host "Task [$exe_id]: /output:$exe_output" -ForegroundColor Red
     $exe_params = $exe_params.Split(" ")
     # echo $exe_params >> $exe_output
     # Start-Process -FilePath bin/TAppEncoder.exe -ArgumentList "$exe_params" >> $exe_output
-    ./TAppEncoder.exe $exe_params >> $exe_output
-    Write-Host "Done  processing ID $id"
+    ./TAppEncoder.exe $exe_params >> $exe_output # 执行编码，写入log文件
+    Write-Host "Done processing ID $id"
 }
 
 $num_finished_task = 0
-$num_task = (Get-Content "task.txt").Length / 3
+$num_task = (Get-Content "task.txt").Length / 3 # task.txt 每个任务占三行
 Write-Host number of tasks: $num_task -foregroundcolor Cyan -backgroundcolor DarkBlue
 $threads = @()
 $handles = @()
- 
+# -le <=
 for ($x = 1; $x -le $num_task; $x++) {
     $powershell = [powershell]::Create().AddScript($ScriptBlock).AddArgument($x)
     $powershell.RunspacePool = $Pool
-    $handles += $powershell.BeginInvoke()
+    $handles += $powershell.BeginInvoke() # 提交异步操作
   $threads += $powershell
 }
 
@@ -45,10 +46,10 @@ do {
   $i = 0
   $done = $true
   foreach ($handle in $handles) {
-    if ($handle -ne $null) {
+    if ($null -ne $handle) {
       if ($handle.IsCompleted) {
-        $threads[$i].EndInvoke($handle)
-        $threads[$i].Dispose()
+        $threads[$i].EndInvoke($handle) # 获得异步执行结果
+        $threads[$i].Dispose() # 销毁
         $handles[$i] = $null
         $num_finished_task += 1
         $Host.UI.RawUI.WindowTitle = '完成' + $num_finished_task + '/' + $num_task
